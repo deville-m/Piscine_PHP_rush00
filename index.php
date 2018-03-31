ac178e4ebc63f898b4213a91fb5b4041a5c9ac97
Clement Trosa <me@trosa.io>
2018-03-31 17:04:39 +0200

    I hate this fucking language.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>CowShop</title>
+  <meta name="description" content="The official cow shopd">
+  <meta name="author" content="42">
+  <link rel="stylesheet" href="css/styles.css">
+  <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+  <div class="header">
+    <div class="wrap">
+        <h2>Cow Shop</h2>
+    </div>
+</div>
+  <script src="js/script.js"></script>
+</body>
+</html>
