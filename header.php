ed28705293abb95a490126d4fd31f7da0cd3ad7c
Robin BARAUD <rbaraud@student.42.fr>
2018-04-01 21:00:35 +0200

    Corrects header pahts

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <body>
   <header>
 	<div class="navbar">
-	  <a class="active" href="index.php">
+	  <a class="active" href="/index.php">
 			<ul class="ul_line">
 				<li class="list_line"><img id="logo" src=/img/logo.svg></li>
 				<li class="list_line" id="brand">Cow Shop</li>
@@ -13,13 +13,13 @@
 	    if (isset($_SESSION)) {
 	        switch ($_SESSION['group']) {
 	        case "admin":
-	            echo '<a class="btn" style="float:right" href="admin.php">'."Administrator".'</a>';
-	            echo '<a class="btn" style="float:right" href="api/logout.php">'.'Logout'.'</a>';
+	            echo '<a class="btn" style="float:right" href="/admin.php">'."Administrator".'</a>';
+	            echo '<a class="btn" style="float:right" href="/api/logout.php">'.'Logout'.'</a>';
 	            break;
 	        case "client":
 							if ($_SESSION['total'])
 							{
-	            	echo "<a class=\"btn\" style=\"float:right\" href=\"panier.php\">
+	            	echo "<a class=\"btn\" style=\"float:right\" href=\"/panier.php\">
 								<ul class=\"ul_line\">
 									<li class=\"list_line\"><img id=\"basket\" src=\"/img/basket.png\" height=\"10px\"></li>
 									<li class=\"list_line\">Panier (".$_SESSION['total']." €)</li>
@@ -27,19 +27,19 @@
 							}
 							else
 							{
-								echo "<a class=\"btn\" style=\"float:right\" href=\"panier.php\">
+								echo "<a class=\"btn\" style=\"float:right\" href=\"/panier.php\">
 								<ul class=\"ul_line\">
 									<li class=\"list_line\"><img id=\"basket\" src=\"/img/basket.png\" height=\"10px\"></li>
 									<li class=\"list_line\">Panier (0 €)</li>
 								</ul></a>";
 							}
-	            echo "<a class=\"btn\" style=\"float:right\" href=\"index.php\">".$_SESSION['logged_on_user']."</a>";
-	            echo '<a class="btn" style="float:right" href="api/logout.php">'.'Logout'.'</a>';
+	            echo "<a class=\"btn\" style=\"float:right\" href=\"/index.php\">".$_SESSION['logged_on_user']."</a>";
+	            echo '<a class="btn" style="float:right" href="/api/logout.php">'.'Logout'.'</a>';
 	            break;
 	        case "":
 							if ($_SESSION['total'])
 							{
-								echo "<a class=\"btn\" style=\"float:right\" href=\"panier.php\">
+								echo "<a class=\"btn\" style=\"float:right\" href=\"/panier.php\">
 								<ul class=\"ul_line\">
 									<li class=\"list_line\"><img id=\"basket\" src=\"/img/basket.png\" height=\"10px\"></li>
 									<li class=\"list_line\">Panier (".$_SESSION['total']." €)</li>
@@ -47,14 +47,14 @@
 							}
 							else
 							{
-								echo "<a class=\"btn\" style=\"float:right\" href=\"panier.php\">
+								echo "<a class=\"btn\" style=\"float:right\" href=\"/panier.php\">
 								<ul class=\"ul_line\">
 									<li class=\"list_line\"><img id=\"basket\" src=\"/img/basket.png\" height=\"10px\"></li>
 									<li class=\"list_line\">Panier (0 €)</li>
 								</ul></a>";
 							}
-	            echo '<a class="btn" style="float:right" href="login.php">'.'Log In'.'</a>';
-	            echo '<a class="btn" style="float:right" href="register.php">'.'Sign Up'.'</a>';
+	            echo '<a class="btn" style="float:right" href="/login.php">'.'Log In'.'</a>';
+	            echo '<a class="btn" style="float:right" href="/register.php">'.'Sign Up'.'</a>';
 	        }
 	    }
 	    ?>
