510caf5f45d49e3769fd917e61d593346b43d360
Clement Trosa <me@trosa.io>
2018-04-01 19:13:13 +0200

    Too tired to write descriptive message

--- panier.php
+++ panier.php
@@ -18,15 +18,28 @@ if ($_SESSION['group'] == "admin") {
 		<div class="header">
 			<?php include("./header.php"); ?>
 		</div>		
-		<span/>
-		<div style="padding-top: 60px;"/>
 		<div class="man-box form">
 			<?php
+			$product = file_to_data(__DIR__ . "/private/product");
+			/* print_r($product); */
+			$list = explode(" ", "toto11 toto12 toto21 toto22");
 			echo '<h2> Panier </h2>';
-			echo '<hr>';
-			echo 'Votre panier est vide';
-			echo '<hr>';
-			echo 'Total: 0€';
+			echo '<hr><br>';
+			$tot = 0;
+			if (!empty($list)) {
+				foreach ($list as $p) {
+					if (!isset($product[$p])) continue;
+					$tot += $product[$p]['price'];
+					echo "<div class=\"textbox\">";
+					echo "<div class=\"left\">Product: <strong>".$p."</strong></div>";
+					echo "<div class=\"right\">Price: <i>".$product[$p]['price']."</i> €</div><br>";
+					echo "</div><br>";
+				}
+			} else {
+				echo '<p>The cart is empty</p>';
+			}
+			echo '<br><hr>';
+			echo 'Total: '.$tot.'€';
 			?>
 		</div>
 	</body>
