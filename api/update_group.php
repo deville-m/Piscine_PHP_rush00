f4f161a55a3fc7fdd21ca4ac625b56f98a34951d
Matthew DEVILLE <mdeville@e2r6p15.42.fr>
2018-04-01 16:11:28 +0200

    Fix update_group

--- api/update_group.php
+++ api/update_group.php
@@ -12,17 +12,18 @@ if ($_SESSION['group'] != "admin") {
     header("Location: ../index.php");
     exit;
 }
+header("Location: ../admin.php");
 if (!$_POST["group"] || !$_POST["user"] || ($data = file_to_data(__DIR__ . PASSWD)) === FALSE)
 	exit;
 if (!$data[$_POST["user"]])
 	exit;
 if ($_POST["group"] === "client")
-	$data[$_POST["user"]] = "client";
+	$data[$_POST["user"]]['group'] = "client";
 if ($_POST["group"] === "admin")
-	$data[$_POST["user"]] = "admin";
+	$data[$_POST["user"]]['group'] = "admin";
 else
 	exit();
-header("Location: ../admin.php");
-}
+if (data_to_file($data, __DIR__ . PASSWD) === FALSE)
+	exit();
 
 ?>
