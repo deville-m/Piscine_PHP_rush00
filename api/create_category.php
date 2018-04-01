482fd8ca9f1f3ee134b9aa5c4e81e53231f572f7
Clement Trosa <me@trosa.io>
2018-04-01 10:52:25 +0200

    Make that it works in 90% of the cases.  3:30.

---
+++ api/create_category.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+include '../backend/database.php'
+
+/*
+ * @author ctrouill
+ * @input name
+ * @input visibility
+ */
+if ($_SESSION['group'] != "admin") {
+    header("HTTP/1.1 401 Unauthorized");
+    header("Location: index.php");
+    exit;
+} else {
+    if ($_POST["name"]) {
+        if (isset($_POST['visibility'])) {
+            add_category($_POST["name"], true);
+        } else {
+            add_category($_POST["name"], false);
+        }
+        header("Location: admin.php");
+        exit;
+    }
+    header("HTTP/1.1 405 Method Not Allowed");
+}
+?>
