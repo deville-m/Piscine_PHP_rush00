3ec9e82010dc2634f25d93e81ac252f48393ae8f
Matthew DEVILLE <mdeville@e2r6p15.42.fr>
2018-04-01 23:30:42 +0200

    add update_info.php

--- api/update_infos.php
+++ api/update_infos.php
@@ -1,22 +1,19 @@
 <?php
 session_start();
-include ('../backend/user.php');
+include_once ('../backend/user.php');
 
 if ($_SESSION['group'] === "admin") {
     header("Location: ../admin.php");
     exit;
 }
 if (($data = file_to_data(__DIR__ . PASSWD)) === FALSE) {
+	header("Location: ../index.php");
 	exit ;
 }
 if ($_POST['new_pseudo'] && $_POST['new_password']) {
-	foreach ($data as $k => $v) {
-		if ($k === $_POST['new_pseudo']) {
-			$data[$_SESSION['logged_on_user']] = $_POST['new_pseudo'];
-			$data[$_POST['new_pseudo']] = $hash("whirlpool", $_POST['new_password']);
-		}
-		$_SESSION['logged_on_user'] = $_POST['new_pseudo'];
-		header("Location: /index.php");
-	}
+	add_user($_POST['new_pseudo'], $_POST['new_password'], "client");
+	remove_user($_SESSION['logged_on_user']);
+	$_SESSION['logged_on_user'] = $_POST['new_pseudo'];
+	header("Location: /index.php");
 }
 ?>
