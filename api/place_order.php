a7d387d97fe81d9c50a71bb7482badd860323e7f
Matthew DEVILLE <mdeville@e2r6p15.42.fr>
2018-04-01 20:32:51 +0200

    place_order.php

--- api/place_order.php
+++ api/place_order.php
@@ -1,19 +1,24 @@
 <?php
 @session_start();
-if ($_SESSION['basket']) {
+
+header("Location: ../index.php");
+if ($_SESSION['basket'] && ($data = file_to_data(__DIR__ . PRODUCT)) !== FALSE) {
 	$tmp = array();
 	$tmp[0] = $_SESSION['logged_on_user'];
 	foreach ($_SESSION['basket'] as $k) {
+		if ($data[$k]['quantity'] == 0)
+			continue;
+		$data[$k]['quantity']--;
 		array_push($tmp, $k);
 	}
-	if (!($orders = file_to_data("../private/order"))) {
+	$_SESSION['basket'] = array();
+	if (empty($tmp) || !($orders = file_to_data("../private/order"))) {
 		exit ;
 	}
 	array_push($order, $tmp);
 	if (!(data_to_file($order, "../private/order"))) {
 		exit ;
 	}
-	$_SESSION['basket'] = array();
 }
-header("Location: /");
+
 ?>
