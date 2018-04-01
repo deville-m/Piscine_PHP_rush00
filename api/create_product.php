290617dbcad297297cc80b556d0a1e0729d27203
Matthew DEVILLE <mdeville@e2r6p15.42.fr>
2018-04-01 22:59:30 +0200

    add product

--- api/create_product.php
+++ api/create_product.php
@@ -3,32 +3,33 @@ session_start();
 
 include_once ('../backend/database.php');
 
-/*
- * @author ctrouill
- * @input name
- * @input visibility
- */
 if ($_SESSION['group'] != "admin") {
     header("HTTP/1.1 401 Unauthorized");
     header("Location: ../index.php");
     exit;
 }
 else if ($_POST["name"] && $_POST["price"] && $_POST["quantity"] && $_POST["image"]) {
-        if (!add_product($_POST["name"], $_POST["price"], $_POST["quantity"], $_POST["image"], isset($_POST['visibility']), isset($_POST["maj"]))) {
-            header("Location: ../admin.php");
-            exit;
-        }
-        $list = get_data_key_list(__DIR__ . CATEGORY);
-        foreach ($list as $kat) {
-            if (isset($_POST[$kat])) {
-                add_product_to_category($kat, $_POST['name']);
-            }
-        }
-        $data = file_to_data(__DIR__ . PRODUCT);
+    if (isset($_POST["visible"]))
+        $visible = true;
+    else
+        $visible = false;
+    if (isset($_POST["maj"]))
+        $maj = true;
+    else
+        $maj = false;
+    if (!add_product($_POST["name"], $_POST["price"], $_POST["quantity"], $_POST["image"], $visible, $maj)) {
         header("Location: ../admin.php");
-	    exit ;
+        exit;
     }
-else {
-    header("HTTP/1.1 405 Method Not Allowed");
+    $list = get_data_key_list(__DIR__ . CATEGORY);
+    foreach ($list as $kat) {
+        if (isset($_POST[$kat])) {
+            add_product_to_category($kat, $_POST['name']);
+        }
+    }
+    $data = file_to_data(__DIR__ . PRODUCT);
+    header("Location: ../admin.php");
+	exit ;
 }
+header("HTTP/1.1 405 Method Not Allowed");
 ?>
