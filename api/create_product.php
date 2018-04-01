72afede6656770f634814da0fce86f0493736220
Clement Trosa <me@trosa.io>
2018-04-01 11:11:11 +0200

    epic

--- api/create_product.php
+++ api/create_product.php
@@ -1,2 +1,26 @@
 <?php
+session_start();
+
+include '../backend/database.php';
+
+/*
+ * @author ctrouill
+ * @input name
+ * @input visibility
+ */
+if ($_SESSION['group'] != "admin") {
+    header("HTTP/1.1 401 Unauthorized");
+    header("Location: ../index.php");
+    exit;
+} else {
+    if ($_POST["name"]
+        && $_POST["price"]
+        && $_POST["quantity"]
+        && $_POST["list"]) {
+        if (isset($_POST['visibility'])) {} else {}
+        header("Location: ../admin.php");
+        exit;
+    }
+    header("HTTP/1.1 405 Method Not Allowed");
+}
 ?>
