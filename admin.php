f456e533220e9e669eaf96c792b8c43667492114
Clement Trosa <me@trosa.io>
2018-04-01 09:26:09 +0200

    formatted all

--- admin.php
+++ admin.php
@@ -1,3 +1,4 @@
+<?php session_start();?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,9 +9,34 @@
 	<link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
+      <header>
+       	<div class="navbar">
+	         <a class="active" href="#">Cow Shop</a>
+	         <a href="index.php">Boutique</a>
+             <?php
+             if (isset($_SESSION)) {
+                 switch ($_SESSION['group']) {
+                 case "admin":
+                     echo '<a style="float:right" href="admin.php">'.$_SESSION['logged_on_user'].'</a>';
+                     echo '<a style="float:right" href="api/logout.php">'.'logout'.'</a>';
+                     break;
+                 case "client":
+                     echo '<a style="float:right" href="panier.php">panier ('."12".'€)</a>';
+                     echo '<a style="float:right" href="index.php">'.$_SESSION['logged_on_user'].'</a>';
+                     echo '<a style="float:right" href="api/logout.php">'.'logout'.'</a>';
+                     break;
+                 case "":
+                     echo '<a style="float:right" href="panier.php">panier ('."0".'€)</a>';
+                     echo '<a style="float:right" href="login.php">'.'log in'.'</a>';
+                     echo '<a style="float:right" href="register.php">'.'sign up'.'</a>';
+                 }
+             }
+             ?>
+	       </div>
+         </header>
 	<div class="cat-box">
 	  <h2>Création catégorie</h2>
-	  <form action="create_category.php" method="post">
+	  <form action="api/create_category.php" method="post">
 		Catégorie: <input type="text" name="name" placeholder="Ex: Useless Things">
 		<input type="checkbox" name="visible" value="visibility"> Visible?
 		<input type="submit"><br>
@@ -19,7 +45,7 @@
 	<hr>
 	<div class="mk-box">
 	  <h2>Création produit</h2>
-	  <form action="create_product.php" method="post" id="mk">
+	  <form action="api/create_product.php" method="post" id="mk">
 		Nom: <input type="text" name="name" placeholder="Ex: Lasso saucisson"><br>
 		Prix: <input type="number" name="prix" min="0"><br>
 	    <?php
@@ -37,7 +63,7 @@
 	<hr>
 	<div class="rm-box">
 	  <h2>Supprimer produit</h2>
-	  <form action="create_category.php" method="post" id="rm">
+	  <form action="api/create_category.php" method="post" id="rm">
 	    <?php
         echo 'Produit: <select name="liste" form="rm">';
 		$list = explode(" ", "ceci est une liste de produits a supprimer");
@@ -53,7 +79,7 @@
 	<hr>
 	<div class="viz-box">
 	  <h2>Rendre Visible</h2>
-	  <form action="make_visible.php" method="post" id="viz">
+	  <form action="api/make_visible.php" method="post" id="viz">
         <?php
         echo 'Produit: <select name="liste" form="viz">';
 		$list = explode(" ", "ceci est une liste");
@@ -76,7 +102,7 @@
 	<hr>
 	<div class="man-box">
       <h2>Gestion Utilisateurs</h2>
-	  <form action="user_manage.php" method="post" id="manage">
+	  <form action="api/user_manage.php" method="post" id="manage">
 		<?php
         echo 'Supprimer compte: <select name="liste" form="manage">';
 		$list = explode(" ", "ceci est une liste");
